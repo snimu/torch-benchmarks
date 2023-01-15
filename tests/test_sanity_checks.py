44f816b952029555dd5f19d033db9b2b1bb9bc68
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-01-15 16:33:10 +0100

    add tests for different `sanity_check_device` & `sanity_check_num_samples`

--- tests/test_sanity_checks.py
+++ tests/test_sanity_checks.py
@@ -1,10 +1,101 @@
 import pytest
 import torch
+import torchvision as tv  # type: ignore[import]
 
-import torch_benchmarks as tbm
 from tests.fixtures.models import SimpleModel
+from torch_benchmarks import benchmark
 
 
 def test_cuda() -> None:
     with pytest.raises(EnvironmentError):
-        _ = tbm.benchmark(SimpleModel, torch.ones(2), loss=torch.nn.CrossEntropyLoss())
+        _ = benchmark(SimpleModel, torch.ones(2), loss=torch.nn.CrossEntropyLoss())
+
+
+@pytest.mark.skipif(
+    not torch.cuda.is_available(), reason="`benchmark` only works on CUDA."
+)
+class TestSanityChecks:
+    """
+    Tests for sanity-checks of torch-benchmarks::benchmark.
+    In class so that `@pytest.mark.skipif(...) only has to be applied once.
+    """
+
+    resnet18 = tv.models.resnet18
+    model_kwargs18 = {"weights": tv.models.ResNet18_Weights.IMAGENET1K_V1}
+    input_data = torch.randn(4, 3, 200, 200)
+    loss = torch.nn.CrossEntropyLoss()
+
+    def test_sanity_check_device(self) -> None:
+        valid_devices = [0, "cuda", torch.device("cuda:0")]
+
+        for device in valid_devices:
+            benchmark(
+                self.resnet18,
+                self.input_data,
+                self.loss,
+                model_kwargs=self.model_kwargs18,
+                device=device,  # type: ignore[arg-type]
+            )
+
+        # Test default arg
+        benchmark(
+            self.resnet18, self.input_data, self.loss, model_kwargs=self.model_kwargs18
+        )
+
+        with pytest.raises(TypeError):
+            benchmark(
+                self.resnet18,
+                self.input_data,
+                self.loss,
+                model_kwargs=self.model_kwargs18,
+                device=(1, 1),  # type: ignore[arg-type]
+            )
+
+        with pytest.raises(ValueError):
+            benchmark(
+                self.resnet18,
+                self.input_data,
+                self.loss,
+                model_kwargs=self.model_kwargs18,
+                device="cpu",
+            )
+
+        with pytest.raises(ValueError):
+            benchmark(
+                self.resnet18,
+                self.input_data,
+                self.loss,
+                model_kwargs=self.model_kwargs18,
+                device=torch.device("cpu"),
+            )
+
+        with pytest.raises(ValueError):
+            benchmark(
+                self.resnet18,
+                self.input_data,
+                self.loss,
+                model_kwargs=self.model_kwargs18,
+                device=torch.cuda.device_count(),
+            )
+
+    def test_sanity_check_num_samples(self) -> None:
+        wrong_inputs = [None, 2.0, "not an int"]
+
+        for wrong_input in wrong_inputs:
+            with pytest.raises(TypeError):
+                benchmark(
+                    self.resnet18,
+                    self.input_data,
+                    self.loss,
+                    model_kwargs=self.model_kwargs18,
+                    num_samples=wrong_input,  # type: ignore[arg-type]
+                )
+
+        with pytest.raises(ValueError):
+            benchmark(
+                self.resnet18,
+                self.input_data,
+                self.loss,
+                model_kwargs=self.model_kwargs18,
+                num_samples=-1,
+            )
