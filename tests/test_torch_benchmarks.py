413b7c1ae58ce3f87ce26e4efa07f75a4fc3514d
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-01-15 12:21:25 +0100

    reorganize test_torch_benchmarks.py to make adding tests easier and less cluttered

--- tests/test_torch_benchmarks.py
+++ tests/test_torch_benchmarks.py
@@ -1,3 +1,4 @@
+# pylint: disable=too-few-public-methods
 import pytest
 import torch
 
@@ -8,6 +9,15 @@ from torch_benchmarks import benchmark
 @pytest.mark.skipif(
     not torch.cuda.is_available(), reason="`benchmark` only works on CUDA."
 )
-def test_doesnt_measure_cuda_itself() -> None:
-    result = benchmark(SimpleModel, input_data=torch.ones(2), loss=torch.nn.BCELoss())
-    assert result.memory_bytes_forward_backward < 1e6
+class TestTorchBenchmarks:
+    """
+    Tests for torch-benchmarks.
+    In class so that `@pytest.mark.skipif(...) only has to be applied once.
+    """
+
+    @staticmethod
+    def test_doesnt_measure_cuda_itself() -> None:
+        result = benchmark(
+            SimpleModel, input_data=torch.ones(2), loss=torch.nn.BCELoss()
+        )
+        assert result.memory_bytes_forward_backward < 1e6
