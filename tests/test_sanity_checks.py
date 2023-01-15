57d107122c5ffcc929c07386a2fe733769c6800c
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-01-15 15:39:48 +0100

    change loss in test_sanity_checks.py to `CrossEntropyLoss` from `BCELoss`

--- tests/test_sanity_checks.py
+++ tests/test_sanity_checks.py
@@ -7,4 +7,4 @@ from tests.fixtures.models import SimpleModel
 
 def test_cuda() -> None:
     with pytest.raises(EnvironmentError):
-        _ = tbm.benchmark(SimpleModel, torch.ones(2), loss=torch.nn.BCELoss())
+        _ = tbm.benchmark(SimpleModel, torch.ones(2), loss=torch.nn.CrossEntropyLoss())
