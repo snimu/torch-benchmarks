aef667ebfbb6e8ae8ede32dca61844d77743c7f2
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-01-17 09:38:09 +0100

    test that benchmarked model-size + input-size is equal to actual model- + input-size
    - Will influence future design decisions
    - So far, forward only. Will create forward-backward-test later after having checked forward-only test

--- tests/test_torch_benchmarks.py
+++ tests/test_torch_benchmarks.py
@@ -1,4 +1,3 @@
-# pylint: disable=too-few-public-methods
 import pytest
 import torch
 
@@ -21,3 +20,37 @@ class TestTorchBenchmarks:
             SimpleModel, input_data=torch.ones(2), loss=torch.nn.CrossEntropyLoss()
         )
         assert result.memory_bytes_forward_backward < 1e6
+
+    @staticmethod
+    def test_tensor_size_plus_model_size_is_full_size_forward_only() -> None:
+        device = torch.device("cuda:0")
+        model_type = torch.nn.Linear
+        model_args = (10,)
+        loss = torch.nn.BCELoss()
+
+        # measure full size
+        bytes_before = torch.cuda.max_memory_allocated(device)
+
+        with torch.no_grad():
+            input_data = torch.ones(10).to(device)
+            model = model_type(*model_args)  # type: ignore[call-arg]
+            model(input_data)
+
+        bytes_full_measurement = torch.cuda.max_memory_allocated(device) - bytes_before
+        del model, input_data
+
+        model_statistics = benchmark(
+            model_type, torch.ones(10), loss, model_args=model_args
+        )
+
+        bytes_before = torch.cuda.max_memory_allocated(device)
+        input_data = torch.ones(10).to(device)
+        bytes_input_data = torch.cuda.max_memory_allocated(device) - bytes_before
+        del input_data
+
+        bytes_full_benchmark = bytes_input_data + model_statistics.memory_bytes_forward
+
+        assert (
+            abs(bytes_full_benchmark - bytes_full_measurement) / bytes_full_measurement
+            < 0.01
+        )
